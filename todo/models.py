f1d7f6785b0626f795747730c7025bb3c8facbde
Taras Andruschenko <And999110@gmail.com>
2022-11-14 16:22:55 +0200

    Define magic method for Tag model & change datetime/deadline fields

--- todo/models.py
+++ todo/models.py
@@ -4,11 +4,14 @@ from django.db import models
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Task(models.Model):
     content = models.TextField(default="Please, fill me in")
-    datetime = models.DateField(auto_now_add=True)
-    deadline = models.DateField(blank=True, null=True)
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(blank=True, null=True)
     status = models.BooleanField(default=False)
     tags = models.ManyToManyField(Tag, related_name="tasks", )
 
