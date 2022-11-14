d061e62f60d37dc21516b07073b339376100e75a
Taras Andruschenko <And999110@gmail.com>
2022-11-14 12:42:08 +0200

    Update urls.py with tags & tasks

--- todo/urls.py
+++ todo/urls.py
@@ -2,11 +2,27 @@ from django.urls import path
 
 from .views import (
     index,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
+    TaskListView,
+    TaskCreateView,
+    TaskUpdateView,
+    TaskDeleteView,
 )
 
 
 urlpatterns = [
     path("", index, name="index"),
+    path("tags/", TagListView.as_view(), name="tag-list"),
+    path("tags/<int:pk>/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks/<int:pk>/create/", TaskCreateView.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 ]
 
 app_name = "todo"
