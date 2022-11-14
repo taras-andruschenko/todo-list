bf9523b610e83f62565408695fbefa07eda39b3a
Taras Andruschenko <And999110@gmail.com>
2022-11-14 11:08:56 +0200

    Add appname to INSTALLED_APPS & create db

--- todo/models.py
+++ todo/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+
 class Task(models.Model):
     content = models.TextField
     datetime = models.DateField(auto_now_add=True)
     deadline = models.DateField(blank=True, null=True)
     status = models.BooleanField(default=False)
-    tags = models.ManyToManyField(related_name="tasks")
+    tags = models.ManyToManyField(Tag, related_name="tasks", )
 
     class Meta:
         ordering = ["datetime"]
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=100)
