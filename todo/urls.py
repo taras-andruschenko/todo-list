3922f9a20c44e638ec8bb433bd0b0c8a6777521c
Taras Andruschenko <And999110@gmail.com>
2022-11-14 11:33:03 +0200

    Create url & index view

--- todo/urls.py
+++ todo/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-# from .views import (
-#     # index,
-# )
+from .views import (
+    index,
+)
 
 
 urlpatterns = [
-    # path("", index, name="index"),
+    path("", index, name="index"),
 ]
 
 app_name = "todo"
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from todo.models import Task, Tag
+
+
+def index(request):
+
+    tasks = Task.objects.all()
+    tags = Tag.objects.all()
+
+    context = {
+        "tasks": tasks,
+        "tags": tags,
+    }
+
+    return render(request, "todo/index.html", context=context)
