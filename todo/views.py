13c92ceb61201065b4654512385c1b50e3507e17
Taras Andruschenko <And999110@gmail.com>
2022-11-14 12:28:52 +0200

    Add CRUD views for Task and Tag models

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
+from todo.forms import TaskForm
 from todo.models import Task, Tag
 
 
@@ -14,3 +17,46 @@ def index(request):
     }
 
     return render(request, "todo/index.html", context=context)
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    queryset = Task.objects.all().prefetch_related("tags")
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:index")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("todo:index")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    success_url = reverse_lazy("todo:index")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("todo:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("todo:tag-list")
